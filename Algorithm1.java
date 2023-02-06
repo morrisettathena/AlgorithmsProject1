e069271b24555367bc623bcba63c3fd1d09eddc9
noyesne <116982639+noyesne@users.noreply.github.com>
2023-02-05 21:23:45 -0500

    Add files via upload
    This has algorithm 1.2 which adds the members in a array. Tested it out and works fine.

---
+++ Algorithm1.java
@@ -0,0 +1,35 @@
+
+public class Algorithm1 {
+
+
+	public Algorithm1(){
+		int[] userArray; 
+	}
+	
+	 public Algorithm1(int[] a){
+		 int[] userArray = a;
+	}
+	
+	public int addElements(int[] userArray){
+		int total = 0;
+		for(int i = 0; i <= userArray.length-1; i++ ) {
+			 total += userArray[i];
+		}
+		return total; 
+	}
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
